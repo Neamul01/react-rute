2eb04dc5ca9c52d7ec70d5a7148b1fa02f0b822d
Neamul01 <neamulhaque2002@gmail.com>
2022-03-29 19:24:03 +0600

    first commit

---
+++ src/component/Friends/Friends.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Friends = () => {
+    return (
+        <div>
+            <h2>Hello this is Friends area !!!!!</h2>
+        </div>
+    );
+};
+
+export default Friends;
